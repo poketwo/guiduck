36b6f36a11b5c748cbbf76e79f2869207368070b
Oliver Ni <oliver.ni@gmail.com>
2020-11-08 22:29:19 -0800

    Add moderation

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,114 +1,170 @@
+import abc
+import typing
 from collections import Counter
 from dataclasses import dataclass
 from datetime import datetime
-from enum import IntEnum
 
 import config
 import discord
-from bson.objectid import ObjectId
-from discord.ext import commands
+from discord.ext import commands, tasks
 from discord.ext.events.utils import fetch_recent_audit_log_entry
+from helpers import time
 
 LOG_CHANNEL = 720552022754983999
 STAFF_ROLE = 721825360827777043
+GUILD_ID = 716390832034414685
 
-
-class ActionType(IntEnum):
-    KICK = 1
-    BAN = 2
-    UNBAN = 3
-    WARN = 4
-    MUTE = 5
-    UNMUTE = 6
-
-    @property
-    def past_tense(self):
-        if self == self.KICK:
-            return "kicked"
-        elif self == self.BAN:
-            return "banned"
-        elif self == self.UNBAN:
-            return "unbanned"
-        elif self == self.WARN:
-            return "warned"
-        elif self == self.MUTE:
-            return "muted"
-        elif self == self.UNMUTE:
-            return "unmuted"
-
-    @property
-    def emoji(self):
-        if self == self.KICK:
-            return "\N{WOMANS BOOTS}"
-        elif self == self.BAN:
-            return "\N{HAMMER}"
-        elif self == self.UNBAN:
-            return "\N{OPEN LOCK}"
-        elif self == self.WARN:
-            return "\N{WARNING SIGN}"
-        elif self == self.MUTE:
-            return "\N{SPEAKER WITH CANCELLATION STROKE}"
-        elif self == self.UNMUTE:
-            return "\N{SPEAKER}"
-
-    @property
-    def color(self):
-        if self == self.KICK:
-            return discord.Color.orange()
-        elif self == self.BAN:
-            return discord.Color.red()
-        elif self == self.UNBAN:
-            return discord.Color.green()
-        elif self == self.WARN:
-            return discord.Color.orange()
-        elif self == self.MUTE:
-            return discord.Color.blue()
-        elif self == self.UNMUTE:
-            return discord.Color.green()
+TimeDelta = typing.Optional[time.TimeDelta]
 
 
 @dataclass
-class Action:
-    target_id: int
-    user_id: int
-    type: ActionType
+class Action(abc.ABC):
+    target: discord.Member
+    user: discord.Member
     reason: str
     created_at: datetime = None
-    _id: ObjectId = None
+    expires_at: datetime = None
+    resolved: bool = None
 
     def __post_init__(self):
         if self.created_at is None:
             self.created_at = datetime.utcnow()
-
-    @property
-    def id(self):
-        return self._id
-
-    def to_dict(self):
-        return dict(
-            target_id=self.target_id,
-            user_id=self.user_id,
-            type=self.type,
-            reason=self.reason,
-            created_at=self.created_at,
-        )
-
-
-@dataclass
-class TempAction(Action):
-    resolved: bool = False
-    expires_at: datetime = None
+        if self.expires_at is not None:
+            self.resolved = False
 
     @property
     def duration(self):
+        if self.expires_at is None:
+            return None
         return self.expires_at - self.created_at
 
     def to_dict(self):
-        return dict(
-            **super().to_dict(),
-            resolved=self.resolved,
-            expires_at=self.expires_at,
+        base = {
+            "target_id": self.target.id,
+            "user_id": self.user.id,
+            "type": self.type,
+            "reason": self.reason,
+            "created_at": self.created_at,
+        }
+        if self.expires_at is not None:
+            base["resolved"] = self.resolved
+            base["expires_at"] = self.expires_at
+        return base
+
+    def to_user_embed(self):
+        embed = discord.Embed(
+            title=f"{self.emoji} {self.past_tense.title()}",
+            description=f"You have been {self.past_tense}.",
+            color=self.color,
         )
+        reason = self.reason or "No reason provided"
+        embed.add_field(name="Reason", value=reason, inline=False)
+        if self.duration is not None:
+            embed.add_field(
+                name="Duration", value=time.strfdelta(self.duration, long=True)
+            )
+            embed.set_footer(text="Expires")
+            embed.timestamp = self.expires_at
+        return embed
+
+    def to_log_embed(self):
+        embed = discord.Embed(color=self.color)
+        embed.set_author(
+            name=f"{self.user} (ID: {self.user.id})", icon_url=self.user.avatar_url
+        )
+        embed.set_thumbnail(url=self.target.avatar_url)
+        embed.add_field(
+            name=f"{self.emoji} {self.past_tense.title()} {self.target} (ID: {self.target.id})",
+            value=self.reason or "No reason provided",
+        )
+        if self.duration is not None:
+            embed.set_footer(
+                text=f"Duration • {time.strfdelta(self.duration, long=True)}\nExpires"
+            )
+            embed.timestamp = self.expires_at
+        return embed
+
+    async def notify(self):
+        try:
+            await self.target.send(embed=self.to_user_embed())
+        except (discord.Forbidden, discord.HTTPException):
+            pass
+
+    @abc.abstractmethod
+    async def execute(self, ctx):
+        pass
+
+
+class Kick(Action):
+    type = "kick"
+    past_tense = "kicked"
+    emoji = "\N{WOMANS BOOTS}"
+    color = discord.Color.orange()
+
+    async def execute(self, ctx):
+        reason = self.reason or f"Action done by {self.user} (ID: {self.user.id})"
+        await ctx.guild.kick(self.target, reason=reason)
+
+
+class Ban(Action):
+    type = "ban"
+    past_tense = "banned"
+    emoji = "\N{HAMMER}"
+    color = discord.Color.red()
+
+    async def execute(self, ctx):
+        reason = self.reason or f"Action done by {self.user} (ID: {self.user.id})"
+        await ctx.guild.ban(self.target, reason=reason)
+
+
+class Unban(Action):
+    type = "unban"
+    past_tense = "unbanned"
+    emoji = "\N{OPEN LOCK}"
+    color = discord.Color.green()
+
+    async def execute(self, ctx):
+        reason = self.reason or f"Action done by {self.user} (ID: {self.user.id})"
+        await ctx.guild.unban(self.target, reason=reason)
+
+
+class Warn(Action):
+    type = "warn"
+    past_tense = "warned"
+    emoji = "\N{WARNING SIGN}"
+    color = discord.Color.orange()
+
+    async def execute(self, ctx):
+        pass
+
+
+class Mute(Action):
+    type = "mute"
+    past_tense = "muted"
+    emoji = "\N{SPEAKER WITH CANCELLATION STROKE}"
+    color = discord.Color.blue()
+
+    async def execute(self, ctx):
+        reason = self.reason or f"Action done by {self.user} (ID: {self.user.id})"
+        role = discord.utils.get(ctx.guild.roles, name="Muted")
+        await self.target.add_roles(role, reason=reason)
+
+
+class Unmute(Action):
+    type = "unmute"
+    past_tense = "unmuted"
+    emoji = "\N{SPEAKER}"
+    color = discord.Color.green()
+
+    async def execute(self, ctx):
+        reason = self.reason or f"Action done by {self.user} (ID: {self.user.id})"
+        role = discord.utils.get(ctx.guild.roles, name="Muted")
+        await self.target.remove_roles(role, reason=reason)
+
+
+@dataclass
+class FakeContext:
+    guild: discord.Guild
 
 
 class BanConverter(commands.Converter):
@@ -132,64 +188,38 @@ class Moderation(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-
-    def to_user_embed(self, action):
-        embed = discord.Embed(
-            title=f"{action.type.emoji} {action.type.past_tense.title()}",
-            description=f"You have been {action.type.past_tense}.",
-            color=action.type.color,
-        )
-        embed.add_field(
-            name="Reason",
-            value=action.reason or "No reason provided",
-            inline=False,
-        )
-        if isinstance(action, TempAction):
-            embed.add_field(name="Duration", value=action.duration)
-        embed.timestamp = action.created_at
-        return embed
+        self.check_actions.start()
 
     async def send_log_message(self, *args, **kwargs):
         channel = self.bot.get_channel(LOG_CHANNEL)
         await channel.send(*args, **kwargs)
 
-    async def send_action_message(self, target, action):
-        try:
-            await target.send(embed=self.to_user_embed(action))
-        except (discord.Forbidden, discord.HTTPException):
-            pass
-
     @commands.Cog.listener()
-    async def on_action_perform(self, target, user, action):
-        embed = discord.Embed(color=action.type.color)
-        embed.set_author(name=f"{user} (ID: {user.id})", icon_url=user.avatar_url)
-        embed.set_thumbnail(url=target.avatar_url)
-        embed.add_field(
-            name=f"{action.type.emoji} {action.type.past_tense.title()} {target} (ID: {target.id})",
-            value=action.reason or "No reason provided",
+    async def on_action_perform(self, action):
+        await self.bot.db.action.update_many(
+            {"target_id": action.target.id, "type": action.type, "resolved": False},
+            {"$set": {"resolved": True}},
         )
-        if isinstance(action, TempAction):
-            embed.set_footer(text=f"Duration: {action.duration}")
-        embed.timestamp = action.created_at
-
         await self.bot.db.action.insert_one(action.to_dict())
-        await self.send_log_message(embed=embed)
+        await self.send_log_message(embed=action.to_log_embed())
 
     @commands.Cog.listener()
     async def on_member_ban(self, guild, target):
+        """Logs ban events not made through the bot."""
+
         entry = await fetch_recent_audit_log_entry(
             self.bot, guild, target=target, action=discord.AuditLogAction.ban, retry=3
         )
         if entry.user == self.bot.user:
             return
 
-        action = Action(
-            target_id=target.id,
-            user_id=entry.user.id,
-            type=ActionType.BAN,
+        action = Ban(
+            target=target,
+            user=entry.user,
             reason=entry.reason,
+            created_at=entry.created_at,
         )
-        self.bot.dispatch("action_perform", target, entry.user, action)
+        self.bot.dispatch("action_perform", action)
 
     @commands.Cog.listener()
     async def on_member_unban(self, guild, target):
@@ -199,26 +229,26 @@ class Moderation(commands.Cog):
         if entry.user == self.bot.user:
             return
 
-        action = Action(
-            target_id=target.id,
-            user_id=entry.user.id,
-            type=ActionType.UNBAN,
+        action = Unban(
+            target=target,
+            user=entry.user,
             reason=entry.reason,
+            created_at=entry.created_at,
         )
-        self.bot.dispatch("action_perform", target, entry.user, action)
+        self.bot.dispatch("action_perform", action)
 
     @commands.Cog.listener()
     async def on_member_kick(self, target, entry):
         if entry.user == self.bot.user:
             return
 
-        action = Action(
-            target_id=target.id,
-            user_id=entry.user.id,
-            type=ActionType.KICK,
+        action = Kick(
+            target=target,
+            user=entry.user,
             reason=entry.reason,
+            created_at=entry.created_at,
         )
-        self.bot.dispatch("action_perform", target, entry.user, action)
+        self.bot.dispatch("action_perform", action)
 
     @commands.command()
     @commands.has_permissions(manage_messages=True)
@@ -246,6 +276,24 @@ class Moderation(commands.Cog):
     @commands.command()
     @commands.guild_only()
     @commands.has_permissions(kick_members=True)
+    async def warn(self, ctx, target: discord.Member, *, reason=None):
+        """Warns a member in the server.
+
+        You must have Kick Members permission to use this.
+        """
+
+        if any(x.id == STAFF_ROLE for x in target.roles):
+            return await ctx.send("You can't punish staff members!")
+
+        action = Warn(target=target, user=ctx.author, reason=reason)
+        await action.notify()
+        await action.execute(ctx)
+        await ctx.send(f"Warned **{target}**.")
+        self.bot.dispatch("action_perform", action)
+
+    @commands.command()
+    @commands.guild_only()
+    @commands.has_permissions(kick_members=True)
     async def kick(self, ctx, target: discord.Member, *, reason=None):
         """Kicks a member from the server.
 
@@ -255,26 +303,18 @@ class Moderation(commands.Cog):
         if any(x.id == STAFF_ROLE for x in target.roles):
             return await ctx.send("You can't punish staff members!")
 
-        action = Action(
-            target_id=target.id,
-            user_id=ctx.author.id,
-            type=ActionType.KICK,
-            reason=reason,
-        )
-
-        await self.send_action_message(target, action)
-        await ctx.guild.kick(
-            target,
-            reason=reason or f"Action done by {ctx.author} (ID: {ctx.author.id})",
-        )
+        action = Kick(target=target, user=ctx.author, reason=reason)
+        await action.notify()
+        await action.execute(ctx)
         await ctx.send(f"Kicked **{target}**.")
-
-        self.bot.dispatch("action_perform", target, ctx.author, action)
+        self.bot.dispatch("action_perform", action)
 
     @commands.command()
     @commands.guild_only()
     @commands.has_permissions(ban_members=True)
-    async def ban(self, ctx, target: discord.Member, *, reason=None):
+    async def ban(
+        self, ctx, target: discord.Member, duration: TimeDelta = None, *, reason=None
+    ):
         """Bans a member from the server.
 
         You must have Ban Members permission to use this.
@@ -283,21 +323,25 @@ class Moderation(commands.Cog):
         if any(x.id == STAFF_ROLE for x in target.roles):
             return await ctx.send("You can't punish staff members!")
 
-        action = Action(
-            target_id=target.id,
-            user_id=ctx.author.id,
-            type=ActionType.BAN,
+        created_at = datetime.utcnow()
+        expires_at = None
+        if duration is not None:
+            expires_at = created_at + duration
+
+        action = Ban(
+            target=target,
+            user=ctx.author,
             reason=reason,
+            created_at=created_at,
+            expires_at=expires_at,
         )
-
-        await self.send_action_message(target, action)
-        await ctx.guild.ban(
-            target,
-            reason=reason or f"Action done by {ctx.author} (ID: {ctx.author.id})",
-        )
-        await ctx.send(f"Banned **{target}**.")
-
-        self.bot.dispatch("action_perform", target, ctx.author, action)
+        await action.notify()
+        await action.execute(ctx)
+        if action.duration is None:
+            await ctx.send(f"Banned **{target}**.")
+        else:
+            await ctx.send(f"Banned **{target}** for **{time.strfdelta(duration)}**.")
+        self.bot.dispatch("action_perform", action)
 
     @commands.command()
     @commands.guild_only()
@@ -308,21 +352,102 @@ class Moderation(commands.Cog):
         You must have Ban Members permission to use this.
         """
 
-        action = Action(
-            target_id=target.user.id,
-            user_id=ctx.author.id,
-            type=ActionType.UNBAN,
-            reason=reason,
-        )
-
-        await self.send_action_message(target.user, action)
-        await ctx.guild.unban(
-            target.user,
-            reason=reason or f"Action done by {ctx.author} (ID: {ctx.author.id})",
-        )
+        action = Unban(target=target.user, user=ctx.author, reason=reason)
+        await action.execute(ctx)
         await ctx.send(f"Unbanned **{target.user}**.")
+        self.bot.dispatch("action_perform", action)
 
-        self.bot.dispatch("action_perform", target.user, ctx.author, action)
+    @commands.command()
+    @commands.guild_only()
+    @commands.has_permissions(kick_members=True)
+    async def mute(
+        self, ctx, target: discord.Member, duration: TimeDelta = None, *, reason=None
+    ):
+        """Mutes a member in the server.
+
+        You must have Kick Members permission to use this.
+        """
+
+        print(duration)
+
+        if any(x.id == STAFF_ROLE for x in target.roles):
+            return await ctx.send("You can't punish staff members!")
+
+        created_at = datetime.utcnow()
+        expires_at = None
+        if duration is not None:
+            expires_at = created_at + duration
+
+        action = Mute(
+            target=target,
+            user=ctx.author,
+            reason=reason,
+            created_at=created_at,
+            expires_at=expires_at,
+        )
+        await action.notify()
+        await action.execute(ctx)
+        if action.duration is None:
+            await ctx.send(f"Muted **{target}**.")
+        else:
+            await ctx.send(f"Muted **{target}** for **{time.strfdelta(duration)}**.")
+        self.bot.dispatch("action_perform", action)
+
+    @commands.command()
+    @commands.guild_only()
+    @commands.has_permissions(kick_members=True)
+    async def unmute(self, ctx, target: discord.Member, *, reason=None):
+        """Unmutes a member in the server.
+
+        You must have Kick Members permission to use this.
+        """
+
+        action = Unmute(target=target, user=ctx.author, reason=reason)
+        await action.execute(ctx)
+        await ctx.send(f"Unmuted **{target}**.")
+        self.bot.dispatch("action_perform", action)
+
+    async def reverse_raw_action(self, raw_action):
+        guild = self.bot.get_guild(GUILD_ID)
+
+        if raw_action["type"] == "ban":
+            action_type = Unban
+            try:
+                ban = await guild.fetch_ban(discord.Object(id=raw_action["target_id"]))
+            except (ValueError, discord.NotFound):
+                return
+            target = ban.user
+        elif raw_action["type"] == "mute":
+            action_type = Unmute
+            target = guild.get_member(raw_action["target_id"])
+        else:
+            return
+
+        action = action_type(
+            target=target,
+            user=self.bot.user,
+            reason="Punishment duration expired",
+            created_at=datetime.utcnow(),
+        )
+
+        await action.execute(FakeContext(guild))
+        await action.notify()
+        self.bot.dispatch("action_perform", action)
+
+    @tasks.loop(seconds=30)
+    async def check_actions(self):
+        await self.bot.wait_until_ready()
+        query = {"resolved": False, "expires_at": {"$lt": datetime.utcnow()}}
+
+        async for action in self.bot.db.action.find(query):
+            self.bot.loop.create_task(self.reverse_raw_action(action))
+
+        await self.bot.db.action.update_many(
+            {"resolved": False}, {"$set": {"resolved": True}}
+        )
+
+    def cog_unload(self):
+        self.check_actions.cancel()
 
 
 def setup(bot):
---
+++ helpers/time.py
@@ -0,0 +1,37 @@
+from datetime import timedelta
+from string import Formatter
+
+from discord.ext import commands
+from durations_nlp import Duration
+
+PERIODS = (
+    ("year", "y", 60 * 60 * 24 * 365),
+    ("month", "M", 60 * 60 * 24 * 30),
+    ("day", "d", 60 * 60 * 24),
+    ("hour", "h", 60 * 60),
+    ("minute", "m", 60),
+    ("second", "s", 1),
+)
+
+
+class TimeDelta(commands.Converter):
+    async def convert(self, ctx, arg):
+        seconds = Duration(arg).to_seconds()
+        if seconds <= 0:
+            raise commands.BadArgument("Invalid duration.")
+        return timedelta(seconds=seconds)
+
+
+def strfdelta(duration, long=False):
+    seconds = int(duration.total_seconds())
+    strings = []
+    for period_name, period_short, period_seconds in PERIODS:
+        if seconds >= period_seconds:
+            period_value, seconds = divmod(seconds, period_seconds)
+            if long:
+                has_s = "s" if period_value > 1 else ""
+                strings.append(f"{period_value} {period_name}{has_s}")
+            else:
+                strings.append(f"{period_value}{period_short}")
+
+    return " ".join(strings)
