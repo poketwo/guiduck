07d29d387463a709055415efb81613a402949e0e
Oliver Ni <oliver.ni@gmail.com>
2021-09-09 10:06:37 -0700

    Add auto catching post

--- cogs/autopost.py
+++ cogs/autopost.py
@@ -1,4 +1,5 @@
 import itertools
+import textwrap
 from collections import defaultdict
 from typing import List
 
@@ -9,32 +10,49 @@ class AutoPost:
     def __init__(self, channels: List[int], message: str, *, each: int = 1):
         self.do_post = itertools.cycle([True] + [False] * (each - 1))
         self.channels = itertools.cycle(channels)
-        self.message = message.strip()
+        self.message = textwrap.dedent(message).strip()
 
-
-PC_MESSAGE = """
-**Reminder:** It is your own responsibility to evaluate trades you make. Once both parties have agreed to and completed a trade, that trade is final. If you change your mind after you make a trade, nothing can nor will be done.
-
-To ensure you get a fair deal, you are highly encouraged to do the following:
-
-• Price all pokémon in the <#722244899767844866> channel.
-• Research trades and auctions with similar pokémon from the past.
-• Ask for other trainers' opinions if unsure about the fairness of trade.
-• Don't give into pressure to buy or sell immediately. You can always try again later.
-• Read through the trading tips document linked in the <#754774504571666534> channel.
-"""
-
-WTS_MESSAGE = """
-**Reminder:** Please put a price on your <#741712512113967214> listing.
-The price must be a single price that you will sell to other users at. "C/O" is not a thing—use auctions for that.
-Check pins for full rules. Advertisements in violation will be removed.
-"""
 
 POSTS = [
     AutoPost(
-        [720040664741576775, 721778540080791569, 721846241696284694, 778139533945602078], PC_MESSAGE
+        [720040664741576775, 721778540080791569, 721846241696284694, 778139533945602078],
+        """
+        **Reminder:** It is your own responsibility to evaluate trades you make. Once both parties have agreed to and completed a trade, that trade is final. If you change your mind after you make a trade, nothing can nor will be done.
+
+        To ensure you get a fair deal, you are highly encouraged to do the following:
+
+        • Price all pokémon in the <#722244899767844866> channel.
+        • Research trades and auctions with similar pokémon from the past.
+        • Ask for other trainers' opinions if unsure about the fairness of trade.
+        • Don't give into pressure to buy or sell immediately. You can always try again later.
+        • Read through the trading tips document linked in the <#754774504571666534> channel.
+        """,
     ),
-    AutoPost([741712512113967214], WTS_MESSAGE, each=6),
+    AutoPost(
+        [741712512113967214],
+        """
+        **Reminder:** Please put a price on your <#741712512113967214> listing.
+        The price must be a single price that you will sell to other users at. "C/O" is not a thing—use auctions for that.
+        Check pins for full rules. Advertisements in violation will be removed.
+        """,
+        each=6,
+    ),
+    AutoPost(
+        [
+            717095398476480562,
+            720020140401360917,
+            720231680564264971,
+            724762012453961810,
+            724762035094683718,
+        ],
+        """
+        **Reminder:** This channel is for catching only.
+
+        • Spamming is not allowed here or anywhere else in the server.
+        • Do not run generic bot commands here, there are multiple channels for that. <#720029048381767751> and <#784148997593890836>
+        • Auctions and market advertisements are not allowed. Use <#741712512113967214> and <#768161635096461362>.
+        """,
+    ),
 ]
 
 
