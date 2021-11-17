632cf0ea3a21cb10f1ba2766bc62258c33024230
Oliver Ni <oliver.ni@gmail.com>
2021-11-17 09:36:05 -0800

    Add submission status to form embed

--- cogs/forms.py
+++ cogs/forms.py
@@ -1,12 +1,35 @@
 import asyncio
 import sys
 import traceback
+from enum import Enum
 
 import discord
 from discord.ext import commands
 from helpers.utils import FakeUser
 
+
+class SubmissionStatus(Enum):
+    UNDER_REVIEW = 0
+    REJECTED = 1
+    ACCEPTED = 2
+    MARKED = 3
+
+
 CHANNEL_ID = 888320631890931732
+
+COLORS = {
+    SubmissionStatus.UNDER_REVIEW.value: None,
+    SubmissionStatus.REJECTED.value: discord.Color.red(),
+    SubmissionStatus.ACCEPTED.value: discord.Color.green(),
+    SubmissionStatus.MARKED.value: discord.Color.blurple(),
+}
+
+TEXT = {
+    SubmissionStatus.UNDER_REVIEW.value: "New Form Submission",
+    SubmissionStatus.REJECTED.value: "Rejected",
+    SubmissionStatus.ACCEPTED.value: "Accepted",
+    SubmissionStatus.MARKED.value: "Marked for Review",
+}
 
 
 class Forms(commands.Cog):
@@ -20,21 +43,31 @@ class Forms(commands.Cog):
         self._task.cancel()
 
     async def send_submission(self, channel, submission):
+        status = submission.get("status", 0)
         user = self.bot.get_user(submission["user_id"]) or FakeUser(submission["user_id"])
+
         embed = discord.Embed(
-            title=f"New Form Submission ({submission['form_id']})",
+            title=f"{TEXT[status]} ({submission['form_id']})",
             url=f"https://forms.poketwo.net/a/{submission['form_id']}/submissions/{submission['_id']}",
-            color=discord.Color.blurple(),
+            color=COLORS[status],
         )
         embed.set_author(name=str(user), icon_url=user.display_avatar.url)
-        await channel.send(embed=embed)
+        embed.set_footer(text=f"User ID • {user.id}")
+
+        if embedded_id := submission.get("embedded_id"):
+            message = await channel.fetch_message(embedded_id)
+            return await message.edit(embed=embed)
+        else:
+            message = await channel.send(embed=embed)
+            await self.bot.mongo.db.submission.update_one(
+                {"_id": submission["_id"]}, {"$set": {"embedded_id": message.id}}
+            )
 
     async def _watch_submissions(self, channel):
-        pipeline = [{"$match": {"operationType": "insert"}}]
-        async with self.bot.mongo.client.support.submission.watch(pipeline) as change_stream:
-            async for change in change_stream:
-                print(change)
-                await self.send_submission(channel, change["fullDocument"])
+        coll = self.bot.mongo.db.submission
+        pipeline = [{"$match": {"operationType": {"$in": ["insert", "replace", "update"]}}}]
+        async for change in coll.watch(pipeline, full_document="updateLookup"):
+            await self.send_submission(channel, change["fullDocument"])
 
     async def watch_submissions(self):
         await self.bot.wait_until_ready()
