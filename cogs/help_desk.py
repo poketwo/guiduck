fd4e0b47f3ece8993c8bfa8b253e5ea6bf98fd5e
WitherredAway <81734495+WitherredAway@users.noreply.github.com>
2022-05-06 13:13:05 +0530

    Re-increase ticket thread archive duration (#28)

--- cogs/help_desk.py
+++ cogs/help_desk.py
@@ -292,6 +292,7 @@ class OpenTicketModal(discord.ui.Modal):
         _id = f"{self.category.id.upper()} {await self.bot.mongo.reserve_id(f'ticket_{self.category.id}'):03}"
         thread = await channel.create_thread(
             name=_id,
+            auto_archive_duration=10080,
             type=discord.ChannelType.private_thread,
             invitable=False,
             reason=f"Created support ticket for {interaction.user}",
