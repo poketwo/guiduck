242a9838852730a68890bd87ebbca14f16afd448
witherr <sdssouvic99@gmail.com>
2024-03-16 16:51:06 +0530

    Modify Paginator to be used when total pages is unknown

--- helpers/pagination.py
+++ helpers/pagination.py
@@ -141,7 +141,7 @@ class Paginator(discord.ui.View):
         The function that takes in the page number and returns what to show for that page.
         It should return either a string or an embed.
     num_pages : int
-        The total number of pages
+        The total number of pages. If not provided, disables last button and page looping.
     loop_pages : bool
         If it should allow looping the pages. If False, it disables first/previous and next/last buttons
         if it's at the beginning and end of the pages, respectively.
@@ -149,9 +149,9 @@ class Paginator(discord.ui.View):
         After how many seconds of inactivity should the paginator stop
     """
 
-    def __init__(self, get_page: Callable[[int], discord.Embed | str], num_pages: int, *, loop_pages: Optional[bool] = True, timeout_after: Optional[int] = 120):
-        self.num_pages = num_pages
+    def __init__(self, get_page: Callable[[int], discord.Embed | str], num_pages: Optional[int] = None, *, loop_pages: Optional[bool] = True, timeout_after: Optional[int] = 120):
         self.get_page = get_page
+        self.num_pages = num_pages
         self.loop_pages = loop_pages
 
         self.current_page = 0
@@ -159,8 +159,24 @@ class Paginator(discord.ui.View):
         self.ctx = None
         super().__init__(timeout=timeout_after)
 
+        if self.num_pages is None:
+            self.last.disabled = True
+            self.loop_pages = False
+
     def is_paginating(self) -> bool:
-        return self.num_pages > 1
+        if self.num_pages is not None:
+            return self.num_pages > 1
+        return True
+
+    def _get_page_kwargs(self, page:  discord.Embed | str) -> Dict[str, discord.Embed | str]:
+        kwargs = {}
+
+        if isinstance(page, discord.Embed):
+            kwargs["embed"] = page
+        elif isinstance(page, str):
+            kwargs["content"] = page
+
+        return kwargs
 
     def _update_labels(self) -> None:
         if not self.is_paginating():
@@ -170,24 +186,24 @@ class Paginator(discord.ui.View):
         if not self.loop_pages:
             pidx = self.current_page
             self.first.disabled = pidx == 0
-            self.last.disabled = (pidx + 1) >= self.num_pages
-            self.next.disabled = (pidx + 1) >= self.num_pages
             self.previous.disabled = pidx == 0
 
-    def _get_page_kwargs(self, page:  discord.Embed | str) -> Dict[str,  discord.Embed | str]:
-        if isinstance(page, discord.Embed):
-            return {"embed": page}
-        if isinstance(page, str):
-            return {"content": page}
+            if self.num_pages is not None:
+                self.next.disabled = (pidx + 1) >= self.num_pages
+                self.last.disabled = (pidx + 1) >= self.num_pages
 
     async def show_page(self, interaction: discord.Interaction, pidx: int) -> None:
+        if self.num_pages is not None:
+            pidx = pidx % self.num_pages
+
         if self.current_page == pidx:
             with contextlib.suppress(discord.NotFound, discord.InteractionResponded):
                 return await interaction.response.defer()
 
         page = await discord.utils.maybe_coroutine(self.get_page, pidx)
         kwargs = self._get_page_kwargs(page)
-        self.current_page = pidx
+        if kwargs:
+            self.current_page = pidx
         self._update_labels()
 
         try:
@@ -198,9 +214,10 @@ class Paginator(discord.ui.View):
 
     async def start(self, ctx: commands.Context, pidx: int = 0):
         self.ctx = ctx
+        page = await discord.utils.maybe_coroutine(self.get_page, pidx)
+        kwargs = self._get_page_kwargs(page)
         self._update_labels()
-        embed = await discord.utils.maybe_coroutine(self.get_page, pidx)
-        self.message = await ctx.reply(embed=embed, view=self, mention_author=False)
+        self.message = await ctx.reply(**kwargs, view=self, mention_author=False)
 
     @discord.ui.button(emoji=FIRST_PAGE_EMOJI, style=discord.ButtonStyle.grey)
     async def first(self, interaction: discord.Interaction, button: discord.Button):
@@ -208,7 +225,7 @@ class Paginator(discord.ui.View):
 
     @discord.ui.button(emoji=PREVIOUS_PAGE_EMOJI, style=discord.ButtonStyle.grey)
     async def previous(self, interaction: discord.Interaction, button: discord.Button):
-        await self.show_page(interaction, (self.current_page - 1) % self.num_pages)
+        await self.show_page(interaction, self.current_page - 1)
 
     @discord.ui.button(emoji=STOP_EMOJI, style=discord.ButtonStyle.grey)
     async def stop_button(self, interaction: discord.Interaction, button: discord.Button):
@@ -222,7 +239,7 @@ class Paginator(discord.ui.View):
 
     @discord.ui.button(emoji=NEXT_PAGE_EMOJI, style=discord.ButtonStyle.grey)
     async def next(self, interaction: discord.Interaction, button: discord.Button):
-        await self.show_page(interaction, (self.current_page + 1) % self.num_pages)
+        await self.show_page(interaction, self.current_page + 1)
 
     @discord.ui.button(emoji=LAST_PAGE_EMOJI, style=discord.ButtonStyle.grey)
     async def last(self, interaction: discord.Interaction, button: discord.Button):
@@ -237,7 +254,7 @@ class Paginator(discord.ui.View):
             timeout=30,
         )
         try:
-            pidx = (int(message.content) - 1) % self.num_pages
+            pidx = (int(message.content) - 1)
         except ValueError:
             return await interaction.followup.send("That's not a valid page number!")
         else:
