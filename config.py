ba4355660668e1c0bb5991d375f58e9ada4a1e87
witherr <sdssouvic99@gmail.com>
2024-10-30 21:57:00 +0530

    Update collections

--- config.py
+++ config.py
@@ -38,10 +38,13 @@ POKETWO_REDIS_PASSWORD = os.getenv("POKETWO_REDIS_PASSWORD")
 OUTLINE_BASE_URL = os.getenv("OUTLINE_BASE_URL")
 OUTLINE_API_TOKEN = os.getenv("OUTLINE_API_TOKEN")
 OUTLINE_COLLECTION_IDS = {
-    "in-progress": "e724cdc4-32a0-4348-af1e-dad2caa50dd2",
-    "information archive": "ce16b7ad-c843-4f10-8d07-817364be3f69",
+    "admins": "845400ae-75bf-44a8-b6ae-5c9be551f30c",
+    "management": "09f803bd-732a-4f4f-8dcf-1d1a4d6e36f0",
+    "bot managers": "a7bfd3b6-85a2-4c85-bd37-23691fa8c499",
+    "server managers": "ba5f0149-dfe5-4922-8b14-be8b33a55577",
     "development": "2edf0a2f-6e73-43f4-be71-34046af0396f",
     "moderators": "fd94ab37-1937-4c50-954e-577916bf0103",
     "moderator wiki": "9b748311-7743-4980-b3ec-4950465be763",
-    "management": "845400ae-75bf-44a8-b6ae-5c9be551f30c"
+    "in-progress": "e724cdc4-32a0-4348-af1e-dad2caa50dd2",
+    "information archive": "ce16b7ad-c843-4f10-8d07-817364be3f69",
 }
