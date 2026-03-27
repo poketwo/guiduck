b5f4669d1ba2e99c35c7ada7b479a384cc4558d6
SammySong <teamsammyutilities@gmail.com>
2026-03-27 23:59:23 +0800

    Update afk.py
    Refer
    <https://github.com/poketwo/guiduck/pull/67#discussion_r2992876001>
    
    Made Enums as per instruction.

--- cogs/afk.py
+++ cogs/afk.py
@@ -1,14 +1,18 @@
 from datetime import datetime
 from typing import Optional
+from enum import Enum
 
 import discord
 from discord.ext import commands
-from discord.ext.menus.views import ViewMenuPages
 
-from helpers.pagination import AsyncEmbedFieldsPageSource
 from helpers import checks
 
 CHAR_LIMIT = 75
+
+class Status(Enum):
+    AFK = "AFK"
+    ONLINE = "Online"
+    DND = "Do Not Disturb"
 
 class Afk(commands.Cog):
     """For AFK status"""
@@ -22,13 +26,13 @@ class Afk(commands.Cog):
         )
 
         if not member_data:
-            return False, "Online", 0
+            return Status.ONLINE.value, Status.ONLINE.value, 0
 
-        afk = member_data.get("afk", False)
-        status = member_data.get("status", "Online")
+        status = member_data.get("status", Status.ONLINE.value)
+        reason = member_data.get("reason", Status.ONLINE.value)
         since = member_data.get("since", 0)
         
-        return afk, status, since
+        return status, reason, since
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -38,11 +42,11 @@ class Afk(commands.Cog):
             return
 
 #        Currently disable may affect Bot's performance      
-#        afk, _, _= await self.get_status(message.author)
-#        if afk:
-#            await self.bot.mongo.db.member.find_one_and_update(
+#        status, _, _= await self.get_status(message.author)
+#        if status == Status.AFK.value:
+#            await self.bot.mongo.db.member.update_one(
 #                {"_id": {"id": message.author.id, "guild_id": message.guild.id}},
-#                {"$set": {"afk": False, "status": "Online", "since": 0}},
+#                {"$set": {"status": Status.ONLINE.value, "reason": Status.ONLINE.value, "since": 0}},
 #                upsert=True,
 #            )
 #            await message.channel.send(f"Welcome back **{message.author.name}**, your AFK status has been removed.")
@@ -51,29 +55,47 @@ class Afk(commands.Cog):
             return
 
         for member in set(message.mentions):
-            afk, status, since = await self.get_status(member)
-            if afk:
-                await message.channel.send(f"User **{member.name}** is currently `{status}` since <t:{since}:R>.")
-
+            status, reason, since = await self.get_status(member)
+            if status == Status.AFK.value:
+                await message.channel.send(f"User **{member.name}** is currently `{reason}` since <t:{since}:R>.")
+            
                 try:
-                    await member.send(f"You have been mentioned in {message.channel} while you were {status}.")
+                    await member.send(f"You have been mentioned in {message.channel} while you were {reason}.\n Jump To: {message.jump_url}")
                 except discord.Forbidden:
                     pass # User unavailable.
+                    
+            elif status == Status.DND:
+                await message.channel.send(f"User **{member.name}** is currently `{reason}` and on **Do Not Disturb**.")
 
     @commands.hybrid_group(fallback="set")
     @commands.guild_only()
-    async def afk(self, ctx, *, status: Optional[str] = "AFK"):
+    async def afk(self, ctx, *, reason: Optional[str] = Status.AFK.value):
         """If no subcommand is called, set your status to AFK"""
 
-        if len(status) > CHAR_LIMIT:
-            return await ctx.send(f"Status too long (max {CHAR_LIMIT} characters).")
-
+        if len(reason) > CHAR_LIMIT:
+            return await ctx.send(f"Reason too long (max {CHAR_LIMIT} characters).")
+        
         await self.bot.mongo.db.member.update_one(
             {"_id": {"id": ctx.author.id, "guild_id": ctx.guild.id}},
-            {"$set": {"afk": True, "status": status, "since": int(datetime.utcnow().timestamp())}},
+            {"$set": {"status": Status.AFK.value, "reason": reason, "since": int(datetime.utcnow().timestamp())}},
             upsert=True,
         )
-        await ctx.send(f"Set user **{ctx.author.name}** status to `{status}`.")
+        await ctx.send(f"Set user **{ctx.author.name}** status to `{reason}`.")
+
+    @afk.command()
+    @commands.guild_only()
+    async def dnd(self, ctx, *, reason: Optional[str] = Status.DND.value):
+        """Set your status on Do Not Disturb"""
+        
+        if len(reason) > CHAR_LIMIT:
+            return await ctx.send(f"Reason too long (max {CHAR_LIMIT} characters).")
+        
+        await self.bot.mongo.db.member.update_one(
+            {"_id": {"id": ctx.author.id, "guild_id": ctx.guild.id}},
+            {"$set": {"status": Status.DND.value, "reason": reason, "since": int(datetime.utcnow().timestamp())}},
+            upsert=True,
+        )
+        await ctx.send(f"Set user **{ctx.author.name}** status to `{reason}` and **Do Not Disturb**.")
 
     @afk.command()
     @commands.guild_only()
@@ -81,7 +103,7 @@ class Afk(commands.Cog):
         """Reset your status"""
         await self.bot.mongo.db.member.update_one(
             {"_id": {"id": ctx.author.id, "guild_id": ctx.guild.id}},
-            {"$set": {"afk": False, "status": "Online", "since": 0}},
+            {"$set": {"status": Status.ONLINE.value, "reason": Status.ONLINE.value, "since": 0}},
             upsert=True,
         )
         await ctx.send(f"Status cleared for user **{ctx.author.name}**.")
@@ -96,7 +118,7 @@ class Afk(commands.Cog):
 
         await self.bot.mongo.db.member.update_one(
             {"_id": {"id": member.id, "guild_id": ctx.guild.id}},
-            {"$set": {"afk": False, "status": "Online", "since": 0}},
+            {"$set": {"status": Status.ONLINE.value, "reason": Status.ONLINE.value, "since": 0}},
             upsert=True,
         )
         await ctx.send(f"Status force cleared for user **{member.name}**.")
@@ -113,15 +135,15 @@ class Afk(commands.Cog):
         """Shows your current status."""
         if member is None:
             member = ctx.author
-        afk, status, since = await self.get_status(member)
+        status, reason, since = await self.get_status(member)
 
         embed = discord.Embed(color=discord.Color.blurple())
         embed.set_author(name=member.display_name, icon_url=member.display_avatar.url)
-        if afk:
-            embed.add_field(name="Status", value=f"{status} since <t:{since}:R>")
+        if status != Status.ONLINE.value:
+            embed.add_field(name=status.value, value=f"{reason} since <t:{since}:R>")
         else:
-            embed.add_field(name="Status", value="Online")
-
+            embed.add_field(name=status.value, value="Online")
+        
         await ctx.send(embed=embed)
 
 async def setup(bot):
